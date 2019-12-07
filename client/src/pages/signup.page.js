193352ecadb98af5a98180eb4dfe8efe6e3d7a9c
MinhTu <minhtunguyenphan@gmail.com>
2019-12-07 20:46:36 +0700

    feat: add route

--- client/src/pages/signup.page.js
+++ client/src/pages/signup.page.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {withRouter,useLocation} from 'react-router-dom';
 // import { useTranslation } from 'react-i18next';
 import SignUp from '../components/SignUp';
-const SignUpPage = ({ t }) => {
+const SignUpPage = ({ setshowLayout }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    async function checkLocation() {
+      if(location.pathname === '/login'||location.pathname === '/signup'){
+        await setshowLayout(false);
+      }
+      else{
+        await setshowLayout(true);
+      }
+    }
+    checkLocation();
+  });
   return (
     <div className="main">
       <SignUp />
@@ -9,4 +23,4 @@ const SignUpPage = ({ t }) => {
   );
 };
 
-export default SignUpPage;
+export default withRouter(SignUpPage);

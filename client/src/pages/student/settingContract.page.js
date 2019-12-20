d79330c5487736218793354cfafd7a4f0fb6807c
haiautruong <haiau762@gmail.com>
2019-12-20 13:00:57 +0700

    block route

--- client/src/pages/student/settingContract.page.js
+++ client/src/pages/student/settingContract.page.js
@@ -4,6 +4,7 @@ import { useHistory, withRouter } from 'react-router-dom';
 import FormContract from '../../components/student/formContract';
 import { Cookies } from 'react-cookie';
 import PropTypes from 'prop-types';
+import * as _ from 'lodash';
 
 const { Sider } = Layout;
 const cookies = new Cookies();
@@ -17,7 +18,9 @@ const SettingContract = ({ setshowLayout }) => {
       await setshowLayout(true);
     }
     fetchLayout();
-  }, []);
+  }, [currTutor]);
+
+  console.log('currTutor', currTutor);
   const navigation = key => {
     if (key === '1') {
       history.push('/student-profile');
@@ -26,35 +29,40 @@ const SettingContract = ({ setshowLayout }) => {
     }
   };
 
-  return (
-    <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
-      <div style={{ background: '#001529' }}>
-        <Sider breakpoint="lg" collapsedWidth="0">
-          <Menu
-            theme="dark"
-            mode="inline"
-            onSelect={({ key }) => navigation(key)}
-          >
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span className="nav-text">Personal Info</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="book" />
-              <span className="nav-text">Your policy</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="message" />
-              <span className="nav-text">Conversation</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+  if (_.isEmpty(currTutor)) {
+    history.push('/');
+    return null;
+  } else {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
+        <div style={{ background: '#001529' }}>
+          <Sider breakpoint="lg" collapsedWidth="0">
+            <Menu
+              theme="dark"
+              mode="inline"
+              onSelect={({ key }) => navigation(key)}
+            >
+              <Menu.Item key="1">
+                <Icon type="user" />
+                <span className="nav-text">Personal Info</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="book" />
+                <span className="nav-text">Your policy</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="message" />
+                <span className="nav-text">Conversation</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+        </div>
+        <div className="container-info">
+          <FormContract tutor={currTutor} />
+        </div>
       </div>
-      <div className="container-info">
-        <FormContract tutor={currTutor} />
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 SettingContract.propTypes = {

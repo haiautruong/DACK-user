dd9b2ee2494dbbafb47dd93399c93fe56daf11ab
duyna5 <duyna5@vng.com.vn>
2019-12-21 16:53:29 +0700

    edit error handler

--- server/app.js
+++ server/app.js
@@ -68,9 +68,10 @@ app.use(function (err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error', {
-        returnMessage: 'Exception Occurred. Retry Later.'
-    });
+    res.json({
+        returnCode: 0,
+        returnMessage: "Exception. Retry Later."
+    })
 });
 
 module.exports = app;

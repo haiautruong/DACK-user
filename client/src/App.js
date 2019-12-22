d0a2453e9e11eab4e825c1fb30cd14f5296f55a8
duyna5 <duyna5@vng.com.vn>
2019-12-22 12:21:11 +0700

    add react GA

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import { Provider } from 'react-redux';
 import { CookiesProvider } from 'react-cookie';
 import AppRouter from './routes';
@@ -7,6 +8,9 @@ import './style/index.scss';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import 'antd/dist/antd.css';
+
+ReactGA.initialize('UA-154863429-1');
+ReactGA.pageview('/');
 
 const App = () => {
   return (
